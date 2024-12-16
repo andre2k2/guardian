f9d509b312e49d4fbbad7ac3bba4dfdc740cc20f
Andre Alvares <andre2k2@gmail.com>
2024-12-16 12:10:35 -0300

    Git ignore

---
+++ src/test/java/com/github/andre2k2/guardian/GuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andre2k2.guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
